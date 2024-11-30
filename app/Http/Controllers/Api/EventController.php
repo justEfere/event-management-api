5eb5b4ae6e25876bc86fd93ebd5eaed780777009
justefere <efereprecious@gmail.com>
2024-11-30 08:06:50 +0100

    basic api response

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class EventController extends Controller
 {
@@ -12,7 +13,9 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        $events = Event::paginate(16);
+
+        return response()->json($events);
     }
 
     /**
@@ -26,9 +29,9 @@ class EventController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Event $event)
     {
-        //
+        return $event;
     }
 
     /**
