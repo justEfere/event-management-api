2ecabda9c07a4174ff6a6587541df26cebf1057b
justefere <efereprecious@gmail.com>
2024-11-30 13:55:04 +0100

    update event

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -53,9 +53,18 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+
+
+        $event->update($request->validate([
+            "name" => ["sometimes", "string", "max:255"],
+            "description" => ["nullable", "string"],
+            "start_time" => ["sometimes", "date"],
+            "end_time" => ["sometimes", "date", "after:start_date"],
+        ]));
+
+        return $event;
     }
 
     /**
